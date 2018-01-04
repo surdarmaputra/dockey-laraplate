c3faf73d6803ee4b053068eebdbaf15985c079e5
sdarmaputra <surya.igede@gmail.com>
2018-01-04 09:22:43 +0700

    WIP: basic role management - add spatie/laravel-permission - add basic authentication - add user controller - seperate factories - create user migration for admin user - include laravel collective and spatie laravel permission service providers

--- app/User.php
+++ app/User.php
@@ -4,10 +4,12 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -26,4 +28,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 }
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,13 +12,8 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
 
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
-    ];
-});
+/*
+ * Please define factories in separated files.
+ * For example create UserFactory.php to store the definition of User Factory, and so on.
+ */
