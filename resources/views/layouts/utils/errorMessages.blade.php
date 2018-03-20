ea09a335de948282929f6310b843a2322bb740f0
sdarmaputra <surya.igede@gmail.com>
2018-03-21 00:11:17 +0700

    add common assets and scripts

--- resources/views/layouts/utils/errorMessages.blade.php
+++ resources/views/layouts/utils/errorMessages.blade.php
@@ -1,7 +1,10 @@
 @if (isset($errors) && count($errors) > 0)
-    <div class="alert alert-danger">
+    <div class="alert alert-danger alert-dismissible fade show">
         @foreach ($errors->all() as $error)
             <div>{{ $error }}</div>
         @endforeach
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
